edc80e22be15d4c1deb1a47f9830ab05e26280bb
Kasper Kilde <kasperkilde2@gmail.com>
2024-05-13 10:38:32 +0200

    fikset de store knappene med informasjon på seg

--- react/src/prefabs/BigButton.tsx
+++ react/src/prefabs/BigButton.tsx
@@ -1,9 +1,36 @@
 export function BigButton() {
     return (
         <>
-            <div className="w-64 h-64 bg-blue-500 rounded-3xl bg-opacity-25 border-white border">
-
+        <div className="flex justify-center gap-8 px-72 flex-wrap">
+        <div className="w-full bg-blue-500 rounded-3xl bg-opacity-25 border-white border text-white text-center h-72 max-w-72 p-8">
+                <div className="flex w-full h-full flex-col justify-center">
+                    <h1 className="text-center text-3xl">Responsive design lorem ipsum dolor sit amet</h1>
+                    <h1 className="text-center">Responsive design lorem ipsum dolor sit amet</h1>
+                    <h1 className="text-center">Responsive design lorem ipsum dolor sit amet</h1>
+                </div>
             </div>
+            <div className="w-full bg-blue-500 rounded-3xl bg-opacity-25 border-white border text-white text-center h-72 max-w-72 p-8">
+                <div className="flex w-full h-full flex-col justify-center">
+                    <h1 className="text-center text-3xl">Responsive design lorem ipsum dolor sit amet</h1>
+                    <h1 className="text-center">Responsive design lorem ipsum dolor sit amet</h1>
+                    <h1 className="text-center">Responsive design lorem ipsum dolor sit amet</h1>
+                </div>
+            </div>
+            <div className="w-full bg-blue-500 rounded-3xl bg-opacity-25 border-white border text-white text-center h-72 max-w-72 p-8">
+                <div className="flex w-full h-full flex-col justify-center">
+                    <h1 className="text-center text-3xl">Responsive design lorem ipsum dolor sit amet</h1>
+                    <h1 className="text-center">Responsive design lorem ipsum dolor sit amet</h1>
+                    <h1 className="text-center">Responsive design lorem ipsum dolor sit amet</h1>
+                </div>
+            </div>
+            <div className="w-full bg-blue-500 rounded-3xl bg-opacity-25 border-white border text-white text-center h-72 max-w-72 p-8">
+                <div className="flex w-full h-full flex-col justify-center">
+                    <h1 className="text-center text-3xl">Responsive design lorem ipsum dolor sit amet</h1>
+                    <h1 className="text-center">Responsive design lorem ipsum dolor sit amet</h1>
+                    <h1 className="text-center">Responsive design lorem ipsum dolor sit amet</h1>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
