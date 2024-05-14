798c8e8d19b7c3860310efff1830e5029344226e
Kasper Kilde <kasperkilde2@gmail.com>
2024-05-14 12:50:36 +0200

    Update Hjemmeside.tsx
    la til at på mobil så ble det fortsatt midstild siden nettsiden var litt offcenter på mobil

--- react/src/Pages/Hjemmeside/Hjemmeside.tsx
+++ react/src/Pages/Hjemmeside/Hjemmeside.tsx
@@ -11,11 +11,11 @@ function Hjemmeside() {
     <Background />
       <Header></Header>
       {/* introduction part */}
-      <div> 
-        <div className='text-center font-bold text-9xl mt-[9rem] text-white'>
+      <div className='text-center text-white'>
+        <div className='font-bold text-4xl md:text-9xl mt-[9rem]'>
           <h1>Cyber Security Services</h1>
         </div>
-        <div className='text-center mt-10 text-gray-400 max-w-screen-sm mx-auto'>
+        <div className='mt-10 text-gray-400 max-w-screen-md mx-auto'>
             <p>Cyber Security Services is a U.S. based cybersecurity company delivering services and managing cyber risk on behalf of our valued distribution and technology partners</p>
         </div>
         <button style={{background: 'linear-gradient(to bottom, #63a9ff, #004d7a)', fontSize: '20px', padding: '10px 40px'}} className='text-white mt-10 mx-auto block rounded-md mb-[11rem]'>Get Started</button>
