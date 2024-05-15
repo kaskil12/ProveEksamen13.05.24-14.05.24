d023557a99c2d0ebdd156dabe0b434d557f63c8d
Kasper Kilde <kasperkilde2@gmail.com>
2024-05-15 12:43:14 +0200

    made changes to the vite.js

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createServer } from 'vite';
+import historyApiFallback from 'connect-history-api-fallback';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    middleware: [
+      historyApiFallback(),
+    ],
+  },
   build: {
-    outDir: '../dist'
-  }
-})
+    outDir: '../dist',
+  },
+});
